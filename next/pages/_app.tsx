ebcc39aaea5641b2bdabeb6da04f7cc79a41e5ec
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-27 13:54:00 +0100

    feat(index): add simple css

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from 'next/app'
+import style from '../styles/index.module.css'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <main>
+        <>
+        <main className={style.main}>
             <Component {...pageProps} />
         </main>
+        <footer className={style.footer}>
+            <p>Created by <a href="https://github.com/augustinmauroy">Augustin Mauroy</a><br />Copyright © 2023 Augustin Mauroy</p>
+        </footer>
+        </>
     )
 };
